8a67ab69cb8d3fbf87e38975badb874bab2dc6f8
Stas <sizovstanislav10@gmail.com>
2023-06-30 16:12:31 +0700

    final v3

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,21 +2,25 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-        int i, j, rows, k = 0;
-        rows= cathetusLength;
-        for ( i =1; i <= rows; i++)
-        {
-            // inner loop define j should be less than equal to rows- i
-            for ( j = 1; j <= rows - i; j++)
-            {
-                System.out.print("  "); // print the space
+        int i, space, rows, k = 0, count = 0, count1 = 0;
+        rows = cathetusLength;
+        for (i = 1; i <= rows; ++i) {
+            for (space = 1; space <= rows - i; ++space) {
+                System.out.print("  ");
+                ++count;
             }
-            // use for loop where k is less than equal to (2 * i -1)
-            for ( k = 1; k <= ( 2 * i - 1); k++)
-            {
-                System.out.printf ("%d ",i); // print the number
+            while (k != 2 * i - 1) {
+                if (count <= rows - 1) {
+                    System.out.printf("%d ", i + k);
+                    ++count;
+                } else {
+                    ++count1;
+                    System.out.printf("%d ", (i + k - 2 * count1));
+                }
+                ++k;
             }
-            System.out.print ("\n");
+            count1 = count = k = 0;
+            System.out.print("\n");
         }
     }
 
